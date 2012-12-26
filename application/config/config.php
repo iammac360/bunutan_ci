c1a793483dc41f0feb3ece401a5e7f66d703bc04
Iammac <iammac@Lions-Mac.local>
2012-12-27 05:53:31 +0800

    edited config.php added an ecrpytion key

--- application/config/config.php
+++ application/config/config.php
@@ -225,7 +225,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = '6Qb0t7g1a6V99DK63gAo2aAsam1q05L4';
 
 /*
 |--------------------------------------------------------------------------
